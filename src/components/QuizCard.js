08d24dc255ad771ec3d7d09713a7576d18b2330f
Achilles-Dev <solohag92@gmail.com>
2023-01-27 18:03:20 +0000

    Makepage responsive

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -6,16 +6,28 @@ import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 const QuizCard = ({ number }) => {
     const [status, setStatus] = useState('Take the test');
     const [statusIcon, setStatusIcon] = useState(faArrowAltCircleRight);
+    const [iconColor, setIconColor] = useState('blue');
 
+    /* To dos
+        1. Get the score for a test and set status to the score and appropriate text
+        2. Set status icon according to the following
+            Unsolved test: Default
+            Failed test: setStatus to Eg. 5/30 correct answers,
+                         setStatusIcon to faTimesCircle
+                         setIconColor to red
+            Passed test: setStatus to Eg. 26/30 correct answers
+                         setStatusIcon to faCheckCircle
+                         setIconColor to green
+    */
     return (
-        <Link to="/test">
-            <section className="flex flex-col gap-3 border-[1px] border-[#0066cc] p-2 max-w-[350px]">
+        <Link to="/test" className="max-w-[350px]">
+            <section className="flex flex-col gap-3 border-[1px] p-2 border-[#0066cc]">
                 <div className="flex justify-center">
                     <h4 className="p-2 border-b-[1px] font-bold">British Citizenship and History Test {number}</h4>
                 </div>
                 <div className="flex justify-between ml-3 mr-2">
                     <h4>{status}</h4>
-                    <FontAwesomeIcon icon={statusIcon} size="2x" color="blue"/>
+                    <FontAwesomeIcon icon={statusIcon} size="2x" color={iconColor}/>
                 </div>
             </section>
         </Link>
--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -9,10 +9,13 @@ const Quiz = () => {
             </section>
             <section className="flex flex-col gap-[20px] mt-[20px] px-[10%] pt-[10px] pb-[40px]">
                 <h3>British Citizenship and History</h3>
-                <div className="flex gap-[20px]">
+                <div className="flex flex-col lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 gap-[20px]">
                     <QuizCard number={1} />
                     <QuizCard number={2} />
                     <QuizCard number={3} />
+                    <QuizCard number={4} />
+                    <QuizCard number={5} />
+                    <QuizCard number={6} />
                 </div>
             </section>
         </main>
