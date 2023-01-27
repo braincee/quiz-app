21576aba71b06a74e1f5606c76605211c2248380
braincee <kwesi938@gmail.com>
2023-01-27 12:11:16 +0000

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Test from './components/Test';
@@ -9,15 +9,15 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div>
-      <Router>
+    <Router>
       <Navbar />
-      <div>
+      <Routes>
         <Route exact path="/" component={Home} />
         <Route path="/test" component={Test} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
-      </div>
-      </Router>
+      </Routes>
+    </Router>
     </div>
     
   );
