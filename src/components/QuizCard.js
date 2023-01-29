918afc54d8cd304a2dbee0f53797aa51497838b7
Achilles-Dev <solohag92@gmail.com>
2023-01-29 17:42:22 +0000

    Add data.json || Update QuizCard with data

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,32 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowAltCircleRight, faTimesCircle, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
-const QuizCard = ({ number }) => {
-    const [status, setStatus] = useState('Take the test');
+const QuizCard = ({ data, questionTotal }) => {
+    const [quizStatus, setQuizStatus] = useState('Take the test');
     const [statusIcon, setStatusIcon] = useState(faArrowAltCircleRight);
     const [iconColor, setIconColor] = useState('blue');
+    const { title, status, score } = data;
 
-    /* To dos
-        1. Get the score for a test and set status to the score and appropriate text
-        2. Set status icon according to the following
-            Unsolved test: Default
-            Failed test: setStatus to Eg. 5/30 correct answers,
-                         setStatusIcon to faTimesCircle
-                         setIconColor to red
-            Passed test: setStatus to Eg. 26/30 correct answers
-                         setStatusIcon to faCheckCircle
-                         setIconColor to green
-    */
+    useEffect(() => {
+        if (status === 'failed') {
+            setStatusIcon(faTimesCircle);
+            setQuizStatus(`${score} / ${questionTotal} correct answers`);
+            setIconColor('red')
+        } else if (status === 'passed') {
+            setStatusIcon(faCheckCircle);
+            setQuizStatus(`${score} / ${questionTotal} correct answers`);
+            setIconColor('green')
+        }
+    }, [status])
+                
+
     return (
         <Link to="/test" className="max-w-[350px]">
             <section className="flex flex-col gap-3 border-[1px] p-2 border-[#0066cc]">
                 <div className="flex justify-center">
-                    <h4 className="p-2 border-b-[1px] font-bold">British Citizenship and History Test {number}</h4>
+                    <h4 className="p-2 border-b-[1px] font-bold">{title}</h4>
                 </div>
                 <div className="flex justify-between ml-3 mr-2">
-                    <h4>{status}</h4>
+                    <h4>{quizStatus}</h4>
                     <FontAwesomeIcon icon={statusIcon} size="2x" color={iconColor}/>
                 </div>
             </section>
--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import QuizCard from '../components/QuizCard';
+import Data from '../data.json';
+
 
 const Quiz = () => {
+    const quizes = Data.quizes;
     return (
         <main>
             <section className="flex bg-[#2874fc] text-white text-[25px] py-[20px] pl-[10%] ">
@@ -10,12 +13,9 @@ const Quiz = () => {
             <section className="flex flex-col gap-[20px] mt-[20px] px-[10%] pt-[10px] pb-[40px]">
                 <h3>British Citizenship and History</h3>
                 <div className="flex flex-col lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 gap-[20px]">
-                    <QuizCard number={1} />
-                    <QuizCard number={2} />
-                    <QuizCard number={3} />
-                    <QuizCard number={4} />
-                    <QuizCard number={5} />
-                    <QuizCard number={6} />
+                    { quizes.length > 0 ? quizes.map((quiz) => (
+                        <QuizCard key={quiz.id} data={quiz} questionTotal={Data.questions[quiz.id].length} />
+                    )) : ""}
                 </div>
             </section>
         </main>
